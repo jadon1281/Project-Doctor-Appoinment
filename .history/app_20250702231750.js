633456501148913f0960c99fd4585da85f0655b3
Prashant <prashantjadon1515@gmail.com>
2025-07-03 00:44:20 +0530

    update message

---
+++ .history/app_20250702231750.js
@@ -0,0 +1,30 @@
+import express from 'express';
+const express = require('express'); // ✅
+
+import {config} from "dotenv";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import fileupload from 
+
+const app = express();
+config({path: "./config/config.env"});
+
+app.use(cors({
+    origin:[process.env.FRONTEND_URL,process.env.DASHBOARD_URL],
+    methods:["GET","POST","PUT","DELETE"],
+    credentials:true,
+})
+);
+
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+app.use(fileupload({
+    useTempFiles: true,
+    tempFileDir: '/tmp/',
+}));
+
+dbconnection();
+
+export default app;
